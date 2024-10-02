1554fcc155aa6b9b1d472daf3274776976d13f26
Jorge Lameiras <66096179+jorgelameiras@users.noreply.github.com>
2024-10-01 20:37:47 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message", "form__message--error");
+    messageElement.classList,add(`form__message--${type}`);
+}
+
+//setFormMessage(loginForm, "success", "You're logged in!");
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+
+    })
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+
+    })
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform AJAX/Fetch login here
+
+        setFormMessage(loginForm, "error", "Invalid username/password comnination");
+    })
+});
